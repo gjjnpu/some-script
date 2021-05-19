1d19d577ba03f34e3c30b216ce029aa40ef6644a
gaojunjienpu <gaojunjienpu@163.com>
2021-05-19 22:12:28 +0800

    update

--- my_send_email.py
+++ my_send_email.py
@@ -5,17 +5,18 @@ from email.mime.text import MIMEText
 
 smtpserver = "smtp.163.com" 
 
-sender = "gaojunjienpu@163.com" 
+sender = "******@163.com" 
 psw = "***" # not password, it should be authorization code 
 
-#psw = "suruo123" 
-receiver = "463901631@qq.com"
+receiver = "******@qq.com"
 
 subject = "this is an example form 163"
 
 msg = MIMEText('hello send by python', "plain", "utf-8")
+
+# below format <>
 msg['From'] = 'frome email <%s>'%sender
-msg['To'] = "to <463901631@qq.com>"
+msg['To'] = "to <"******@qq.com>"
 msg['Subject'] = subject
 
 smtp = smtplib.SMTP()
